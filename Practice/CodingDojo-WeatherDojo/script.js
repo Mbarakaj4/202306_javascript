501bd616b517291a72fe9f4dd53462b10d5343f7
mbarakaj4 <113691935+Mbarakaj4@users.noreply.github.com>
2023-07-05 15:01:16 -0400

    Update WeatherDojo: added functionalities and finish the page

--- Practice/CodingDojo-WeatherDojo/script.js
+++ Practice/CodingDojo-WeatherDojo/script.js
@@ -2,6 +2,25 @@ function cAlert(){
     alert(`Loading weather report...`)
 }
 
-function tempSel(e) {
-
+function cAccept(){
+    $(".cookie-banner").css("display","none");
 }
+
+function cToF(celsius) {
+    var fahrenheit = (celsius * 9) / 5 + 32;
+    return fahrenheit;
+  }
+  
+  function fToC(fahrenheit) {
+    var celsius = ((fahrenheit - 32) * 5) / 9;
+    return celsius;
+  }
+
+  $("#temp").change(function () {
+    if (this.value === "celsius") {
+        console.log($(".min").text());
+    }
+    else if (this.value === "fahrenheit") {
+        console.log(min.innerHTML);
+    }
+  })
