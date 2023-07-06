8e5a5d3bd56602c3b472316cf99d48a8a1789b22
mbarakaj4 <113691935+Mbarakaj4@users.noreply.github.com>
2023-07-06 15:05:58 -0400

    Update WeatherDojo
    Add temperature converting functions, fix the temp colors in css
    and add a class to temp symbols

--- Practice/CodingDojo-WeatherDojo/script.js
+++ Practice/CodingDojo-WeatherDojo/script.js
@@ -1,26 +1,37 @@
-function cAlert(){
-    alert(`Loading weather report...`)
+function cAlert() {
+  alert(`Loading weather report...`)
 }
 
-function cAccept(){
-    $(".cookie-banner").css("display","none");
+function cAccept() {
+  $(".cookie-banner").css("display", "none");
 }
 
-function cToF(celsius) {
-    var fahrenheit = (celsius * 9) / 5 + 32;
-    return fahrenheit;
-  }
-  
-  function fToC(fahrenheit) {
-    var celsius = ((fahrenheit - 32) * 5) / 9;
-    return celsius;
-  }
+function fahrenheitDraw(c, i) {
+  var f = parseInt((c * 9) / 5 + 32);
+  $(".card-temp")[i].innerText = f;
+  $(".card-symbol")[i].innerText = "ºF";
+}
 
-  $("#temp").change(function () {
-    if (this.value === "celsius") {
-        console.log($(".min").text());
-    }
-    else if (this.value === "fahrenheit") {
-        console.log(min.innerHTML);
-    }
-  })
+function celsiusDraw(f, i) {
+  var c = Math.round(parseInt(((f - 32) * 5) / 9) + 1);
+  $(".card-temp")[i].innerText = c;
+  $(".card-symbol")[i].innerText = "ºC";
+}
+
+function extractValuesF (v, i) {
+  fahrenheitDraw(v.innerText, i);
+}
+
+function extractValuesC (v, i) {
+  celsiusDraw(v.innerText, i);
+}
+
+$("#temp").change(function () {
+  var tempArr = document.querySelectorAll(".card-temp");
+  if (this.value === "fahrenheit") {
+    tempArr.forEach(extractValuesF);
+  }
+  else if (this.value === "celsius") {
+    tempArr.forEach(extractValuesC);
+  }
+})
